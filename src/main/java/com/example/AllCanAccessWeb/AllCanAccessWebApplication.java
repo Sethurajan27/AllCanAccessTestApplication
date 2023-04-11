7bf9c11bed637cb459007bc4ee2368ab4e45a81d
Sethurajan27 <106911055+Sethurajan27@users.noreply.github.com>
2023-04-11 10:23:40 +0530

    Update AllCanAccessWebApplication.java

--- src/main/java/com/example/AllCanAccessWeb/AllCanAccessWebApplication.java
+++ src/main/java/com/example/AllCanAccessWeb/AllCanAccessWebApplication.java
@@ -2,11 +2,17 @@ package com.example.AllCanAccessWeb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AllCanAccessWebApplication {
-
+public class AllCanAccessWebApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(AllCanAccessWebApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AllCanAccessWebApplication.class);
 	}
 }
